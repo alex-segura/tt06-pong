c95bac86698d74b687e989f9c5c9e376dc67b11d
Alex Segura <alex@lispm.dev>
2024-02-11 11:49:11 -0700

    Add paddle range test

--- test/test.py
+++ test/test.py
@@ -2,31 +2,51 @@
 # SPDX-License-Identifier: MIT
 
 import cocotb
+from encoder import Encoder
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge
+
+PADDLE_MAX = 430
 
 @cocotb.test()
-async def test_adder(dut):
+async def test_paddle_range(dut):
   dut._log.info("Start")
-  
-  # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+  clocks_per_phase = 5
+  enc0 = Encoder(dut.clk, dut.ui_in[0], dut.ui_in[1], clocks_per_phase=clocks_per_phase, noise_cycles=0)
+  enc1 = Encoder(dut.clk, dut.ui_in[2], dut.ui_in[3], clocks_per_phase=clocks_per_phase, noise_cycles=0)
   clock = Clock(dut.clk, 10, units="us")
   cocotb.start_soon(clock.start())
 
-  # Reset
+  # reset
   dut._log.info("Reset")
   dut.ena.value = 1
   dut.ui_in.value = 0
-  dut.uio_in.value = 0
   dut.rst_n.value = 0
   await ClockCycles(dut.clk, 10)
   dut.rst_n.value = 1
 
-  # Set the input values, wait one clock cycle, and check the output
   dut._log.info("Test")
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
+  assert dut.user_project.pong.paddle1_vpos.value == 0
+  assert dut.user_project.pong.paddle2_vpos.value == 0
 
-  await ClockCycles(dut.clk, 1)
+  # increase paddle to max
+  for i in range(clocks_per_phase * 2 * PADDLE_MAX):
+    await enc0.update(1)
+    await enc1.update(1)
 
-  assert dut.uo_out.value == 50
+  # wait for vsync
+  await RisingEdge(dut.user_project.vsync)
+  await FallingEdge(dut.clk)
+  assert dut.user_project.pong.paddle1_vpos.value.integer == PADDLE_MAX
+  assert dut.user_project.pong.paddle2_vpos.value.integer == PADDLE_MAX
+
+  # decrease paddle to min
+  for i in range(clocks_per_phase * 2 * PADDLE_MAX):
+    await enc0.update(-1)
+    await enc1.update(-1)
+
+  # wait for vsync
+  await RisingEdge(dut.user_project.vsync)
+  await FallingEdge(dut.clk)
+  assert dut.user_project.pong.paddle1_vpos.value.integer == 0
+  assert dut.user_project.pong.paddle2_vpos.value.integer == 0
